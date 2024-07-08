b47b33bed0b757e30be331313c046e5ab88b5a17
RAVENA CAMPOS <ravenascampos@gmail.com>
2024-07-08 01:26:09 -0300

    feat(signupOptionsPage): add Container component

---
+++ src/app/signup/components/Container/index.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react';
+import {
+  Container as MuiContainer,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+
+function Container({ children }: { children: React.ReactNode }) {
+  const theme = useTheme();
+  const isSmallDevice = useMediaQuery(theme.breakpoints.down('md'));
+  return (
+    <MuiContainer
+      maxWidth={false}
+      sx={{
+        display: 'flex',
+        flexDirection: isSmallDevice ? 'column' : 'row',
+        height: '100dvh',
+        width: '100%',
+        overflow: 'hidden',
+        padding: !isSmallDevice ? '0 !important' : '16px !important',
+      }}
+    >
+      {!isSmallDevice && (
+        <MuiContainer
+          sx={{
+            flex: 1,
+            backgroundImage: 'url(/images/background-signup.png)',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            height: '100%',
+          }}
+        />
+      )}
+      <MuiContainer
+        sx={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 4,
+        }}
+      >
+        {children}
+      </MuiContainer>
+    </MuiContainer>
+  );
+}
+
+export default Container;
